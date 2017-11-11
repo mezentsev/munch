32a1b769e6b91727f2deb66ae79314ce4843cdcc
v.mezentsev <mezentsevvo@gmail.com>
2017-11-11 17:51:42 +0300

    Migrate to JAVA 1.8

--- lib-omnibox/src/main/java/com/munch/omnibox/view/OmniboxView.java
+++ lib-omnibox/src/main/java/com/munch/omnibox/view/OmniboxView.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.munch.mvp.MvpView;
 import com.munch.omnibox.R;
