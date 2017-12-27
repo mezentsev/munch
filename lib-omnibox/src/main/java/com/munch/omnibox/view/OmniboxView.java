492b83df17126b13c48091d224d7af46c5c4ee12
v.mezentsev <mezentsevvo@gmail.com>
2017-12-27 16:00:32 +0300

    Added cross for suggest

--- lib-omnibox/src/main/java/com/munch/omnibox/view/OmniboxView.java
+++ lib-omnibox/src/main/java/com/munch/omnibox/view/OmniboxView.java
@@ -3,7 +3,11 @@ package com.munch.omnibox.view;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.munch.mvp.MvpView;
@@ -24,6 +28,33 @@ public final class OmniboxView
                        @Nullable AttributeSet attrs,
                        int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        inflate(getContext(), R.layout.munch_omnibox_view, this);
+        View view = inflate(getContext(), R.layout.munch_omnibox_view, this);
+        View crossView = view.findViewById(R.id.munch_omnibox_cross);
+        EditText searchView = view.findViewById(R.id.munch_omnibox_search);
+
+        searchView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() > 0) {
+                    crossView.setVisibility(VISIBLE);
+                } else {
+                    crossView.setVisibility(GONE);
+                }
+            }
+        });
+
+        crossView.setOnClickListener((View v) -> {
+            searchView.setText("");
+        });
     }
 }
